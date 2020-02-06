074bbf45c0775402ca8b091b8efa9f48d76d1320
yassataiseer <56459253+yassataiseer@users.noreply.github.com>
2020-02-05 21:59:14 -0500

    Add files via upload

---
+++ ccc-2015/j1.py
@@ -0,0 +1,16 @@
+a = int (input())
+b= int (input())
+
+if a ==2 and b==18:
+    print("Special")
+elif a<2:
+    print("Before")
+elif a<2:
+    print("After")
+elif a==2 and b>18:
+    print("After")
+else:
+    print("Before")
+
+
+
